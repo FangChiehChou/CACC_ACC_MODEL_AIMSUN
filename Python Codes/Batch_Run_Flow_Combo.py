d008bbc46260ecdb4cbf41c7a15f7fc4c3cf0f0e
Dali Wei_PATH_PC <trafficdali@gmail.com>
2015-09-08 18:40:45 -0700

    Change python code to rerun console for each combination

--- Python Codes/Batch_Run_Flow_Combo.py
+++ Python Codes/Batch_Run_Flow_Combo.py
@@ -47,13 +47,19 @@ def handleArguments(fileName, expId):
         on_ramp = []
         off_ramp = []
         through = []
-        if readflow(on_ramp, off_ramp, through):
+        flow_combo_index = readflow(on_ramp, off_ramp, through)
+        if flow_combo_index >=0:
+            index=0
             for on_ramp_flow in xrange(on_ramp[0], on_ramp[2], (on_ramp[1] if on_ramp[1]>0 else 1)):
                 for off_ramp_flow in xrange(off_ramp[0], off_ramp[2], (off_ramp[1] if off_ramp[1]>0 else 1)):
                     for through_flow in xrange(through[0], through[2],(through[1] if through[1]>0 else 1)):
-                        savetempfile(on_ramp_flow, off_ramp_flow, through_flow)
-                        print "On_ramp: %d, Off_ramp: %d, and Through: %d" % (on_ramp_flow, off_ramp_flow, through_flow)
-                        simulateMicro(model, result)
+                        if index == flow_combo_index:
+                            savetempfile(on_ramp_flow, off_ramp_flow, through_flow)
+                            print "On_ramp: %d, Off_ramp: %d, and Through: %d" % (on_ramp_flow, off_ramp_flow, through_flow)
+                            simulateMicro(model, result)
+                            writenextcomboindex(on_ramp, through, off_ramp, flow_combo_index+1)
+                            console.save(fileName)
+                            console.close()
         else:
             print('Invalid flow file!')
         console.save(fileName)
@@ -69,8 +75,9 @@ def readflow(on_ramp,off_ramp,through):
     text_file.close()
     val = data.split(',')
     try:
-        if len(val) != 9:
-            return False
+        index_flowcombination = 0
+        if len(val) != 10:
+            return -1
         for i in xrange(3):
             on_ramp.append(int(val[i]))
         if on_ramp[0] >= on_ramp[2]:
@@ -89,14 +96,28 @@ def readflow(on_ramp,off_ramp,through):
             off_ramp[0]=0
             off_ramp[1]=1
             off_ramp[2]=1
+        if int(val[9]) == -1:
+            return -1
+        else:
+            return int(val[9])
         print val
     except ValueError:
-        return False
-    return True
+        return -1
 
 def savetempfile(on_ramp_flow,off_ramp_flow, through_flow):
     text_file = open("C:\CACC_Simu_Data\Temp.txt", "w")
     content = '{0},{1},{2}'.format(on_ramp_flow, through_flow, off_ramp_flow)
+    text_file.write(content)
+    text_file.close()
+
+def writenextcomboindex(on_ramp, off_ramp, through, index):
+    text_file = open("C:\CACC_Simu_Data\Batch_Volume.txt", "w")
+
+    content = '{0},{1},{2},'.format(on_ramp[0], on_ramp[1], on_ramp[2])
+    content = content + '{0},{1},{2},'.format(on_ramp[0], on_ramp[1], on_ramp[2])
+    content = content + '{0},{1},{2},'.format(on_ramp[0], on_ramp[1], on_ramp[2])
+    content = content + '{0}'.format(index)
+
     text_file.write(content)
     text_file.close()
 
