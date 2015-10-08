0b02ad69fe5e67f2c5e5f9afbe179907d34c19f8
Dali Wei_PATH_PC <trafficdali@gmail.com>
2015-10-08 10:49:47 -0700

    Update CompareEqu parameters

--- Matlab code/TestCACCModification/CompareEqu.m
+++ Matlab code/TestCACCModification/CompareEqu.m
@@ -11,29 +11,35 @@
 
 clear;
 
-v_max = 40;
-tau = 0.5;
+v_max = 31.3;  %[m/s]
+tau = 0.5; 
 theta = 0.2;
 b_f = -3;
 b_e = -3*1.1;
 
 % for newell
 headway = 1.3;
-jamgap = 1;
-len = 4;
+jamgap = 1.5; %[m]
+len = 4.5; %[m]
 
-for i=10:40
-    speed_newell(i)=min(v_max,(i-jamgap)/headway);
+capacity = 2100; % [vphpl]
+headway = ...
+    (v_max/(capacity/3600)-jamgap-len)/v_max;
+
+for i=6:40
+    speed_newell(i-5)=min(v_max,(i-jamgap-len)/headway);
 end
 
-for i=10:40
-    speed_gipps(i)=min(v_max, ...
+for i=6:40
+    speed_gipps(i-5)=min(v_max, ...
         max((tau+theta)/(1/-b_f-1/(-b_e))...
         *(-1+sqrt(1+(2*(i-(len+jamgap))*(1/-b_f-1/(-b_e)))/(tau+theta)^2)),0));
 end
 
-plot(speed_newell,'r');hold on;
-plot(speed_gipps,'b'); hold off;
+dis = 6:40;
+dis = dis - len;
+plot(dis, speed_newell,'r');hold on;
+plot(dis, speed_gipps,'b'); hold off;
 
 
 
